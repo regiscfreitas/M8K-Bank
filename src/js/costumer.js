5d44f9eadee0e8597f936653688f7e3bcc2f76da
regiscfreitas <regiscfreitas1212@gmail.com>
2023-05-22 18:11:14 -0300

    trying to return the updated balance after a withdraw

--- src/js/costumer.js
+++ src/js/costumer.js
@@ -5,9 +5,26 @@ let loggedUserHtml = document.getElementById("costumerLogged");
 loggedUserHtml.innerHTML = userSelected.name;
 
 let balance = document.getElementById("balance");
-balance.value = `R$ ${userSelected.balance[0].currentBalance.toFixed(2)}`;
+balance.value = `R$ ${Number(
+  userSelected.balance[0].currentBalance.toFixed(2)
+)}`;
 
 let withdraw = document.getElementById("withdraw");
 let deposit = document.getElementById("deposit");
+let myForm = document.getElementById("costumerForm");
 
 //criar o form com o action para a mesma pagina, recebendo os valores atualizados
+
+console.log(balance.value);
+document.addEventListener("DOMContentLoaded", function () {
+  if (withdraw.value) {
+    Number(balance.value) -= Number(withdraw.value);
+    alert(balance.value);
+  }
+});
+
+myForm.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    //nome da funcao(event);
+  }
+});
