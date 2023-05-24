ab9ebc5397595cd629d4bc27ecf4e9a245cc9a28
regiscfreitas <regiscfreitas1212@gmail.com>
2023-05-24 18:19:39 -0300

    adding style on costumer.html

--- src/js/costumer.js
+++ src/js/costumer.js
@@ -1,25 +1,24 @@
 let userSelected = JSON.parse(localStorage.getItem("userSelectedData"));
-console.log(userSelected);
 
 let loggedUserHtml = document.getElementById("costumerLogged");
 loggedUserHtml.innerHTML = userSelected.name;
 
 let balance = document.getElementById("balance");
-balance.value = `R$ ${Number(
-  userSelected.balance[0].currentBalance.toFixed(2)
-)}`;
+balance.value = Number(userSelected.balance[0].currentBalance.toFixed(2));
 
 let withdraw = document.getElementById("withdraw");
 let deposit = document.getElementById("deposit");
 let myForm = document.getElementById("costumerForm");
+let operation = document.getElementById("operation");
 
-//criar o form com o action para a mesma pagina, recebendo os valores atualizados
+document.addEventListener("DOMContentLoaded", function () {});
 
-console.log(balance.value);
-document.addEventListener("DOMContentLoaded", function () {
-  if (withdraw.value) {
-    Number(balance.value) -= Number(withdraw.value);
-    alert(balance.value);
+operation.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (withdraw.value !== undefined) {
+    console.log(balance.value);
+    console.log(withdraw.value);
+    balance.value -= withdraw.value;
   }
 });
 
